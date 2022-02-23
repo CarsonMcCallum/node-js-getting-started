e73f985cd6f2695ebcf7122a620f49b6daf0666e
CarsonMcCallum <67606896+CarsonMcCallum@users.noreply.github.com>
2022-02-23 15:15:03 -0800

    change port launch

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const path = require('path')
-const PORT = 3000
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  //.listen(process.env.PORT || 3000)
+  //.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(port, host, function() {
+    console.log("Server started.......");
+  });
